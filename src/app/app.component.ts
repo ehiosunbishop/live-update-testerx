e0f69360aaf89c1468e9c749921a2d1d01f22a06
Ehiosun Bishop <ehiosunbishop@gmail.com>
2024-03-16 22:56:31 +0100

    push update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CapacitorUpdater } from '@capgo/capacitor-updater';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.initApp()
+  }
+
+  initApp() {
+    CapacitorUpdater.notifyAppReady();
+
+    App.addListener('appStateChange', async (state) => {
+      let version;
+      if (state.isActive) {
+        // Ensure download occurs while the app is active, or download may fail
+        version = await CapacitorUpdater.download({
+          url: 'https://github.com/Cap-go/demo-app/releases/download/0.0.4/dist.zip',
+          version: '0.0.4'
+        })
+      }
+
+      if (!state.isActive && version) {
+        // Activate the update when the application is sent to background
+        //SplashScreen.show()
+        try {
+          await CapacitorUpdater.set(version);
+          // At this point, the new version should be active, and will need to hide the splash screen
+        } catch {
+          //SplashScreen.hide() // Hide the splash screen again if something went wrong
+        }
+      }
+    })
+  }
 }
