b2f1971c5fee1b68d267b65e96a32be9b2705f2e
Ehiosun Bishop <ehiosunbishop@gmail.com>
2024-03-19 23:07:14 +0100

    Live update test POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,28 @@ import { App } from '@capacitor/app';
 })
 export class AppComponent {
   constructor() {
-    this.initApp()
+    this.initApp();
+    this.addCapUpdaterListeners();
   }
+
+  async addCapUpdaterListeners() {
+    await CapacitorUpdater.addListener('download', data => {
+      console.log("Download event received:", data);
+    });
+
+    await CapacitorUpdater.addListener('noNeedUpdate', data => {
+      console.log("No need update event received:", data);
+    });
+
+    await CapacitorUpdater.addListener('updateFailed', data => {
+      console.log("Update failed event received:", data);
+    });
+
+    await CapacitorUpdater.addListener('downloadFailed', data => {
+      console.log("Download failed event received:", data);
+    });
+  }
+
 
   initApp() {
     CapacitorUpdater.notifyAppReady();
@@ -18,21 +38,16 @@ export class AppComponent {
     App.addListener('appStateChange', async (state) => {
       let version;
       if (state.isActive) {
+        console.log('Got Here');
         // Ensure download occurs while the app is active, or download may fail
         version = await CapacitorUpdater.download({
-          url: 'https://github.com/Cap-go/demo-app/releases/download/0.0.4/dist.zip',
-          version: '0.0.4'
-        })
-      }
+          url: 'https://github.com/ehiosunbishop/live-update-testerx/raw/main/www.zip',
+          version: '0.0.6'
+        });
 
-      if (!state.isActive && version) {
-        // Activate the update when the application is sent to background
-        //SplashScreen.show()
-        try {
+        if (version) {
+          console.log('setting in the value: ');
           await CapacitorUpdater.set(version);
-          // At this point, the new version should be active, and will need to hide the splash screen
-        } catch {
-          //SplashScreen.hide() // Hide the splash screen again if something went wrong
         }
       }
     })
